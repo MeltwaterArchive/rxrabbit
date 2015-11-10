1cd182a4a616faeb106a7a71733a405e9fbabe4b
Pelle Berglund <per.berglund@meltwater.com>
2015-11-10 12:25:04 +0100

    Added bulk support to rx-rabbit-meltwater
    	- Bulks are sent in tar.gz format
    	- Adds header file as first file in tar archive

--- rx-rabbit/src/main/java/com/meltwater/rxrabbit/ConsumerFactory.java
+++ rx-rabbit/src/main/java/com/meltwater/rxrabbit/ConsumerFactory.java
@@ -1,73 +1,8 @@
 package com.meltwater.rxrabbit;
 
-import com.meltwater.rxrabbit.impl.DefaultChannelFactory;
-import com.meltwater.rxrabbit.impl.SingleChannelConsumer;
-import com.meltwater.rxrabbit.util.Logger;
 import rx.Observable;
-import rx.Scheduler;
-import rx.schedulers.Schedulers;
 
-import java.util.ArrayList;
-import java.util.List;
+public interface ConsumerFactory {
 
-/**
- * Can create {@link Observable}s that streams the messages delivered to the connected rabbit queue.
- *
- * Note that a single {@link ChannelFactory} is backing all the {@link Observable}s created by this factory,
- * so if the {@link DefaultChannelFactory} is used it means that all observables will share the same {@link com.rabbitmq.client.Connection}
- * but use different {@link com.rabbitmq.client.Channel}s.
- *
- */
-public class ConsumerFactory {
-
-    private final static Logger log = new Logger(ConsumerFactory.class);
-
-    private final ChannelFactory channelFactory;
-    private final ConsumerSettings settings;
-
-    private ConsumeEventListener consumeEventListener = getConsumeEventListener();
-
-    private Scheduler consumerObserveOnScheduler = Schedulers.computation();
-
-    public ConsumerFactory(ChannelFactory channelFactory, ConsumerSettings settings) {
-        this.settings = settings;
-        this.channelFactory = channelFactory;
-    }
-
-    public ConsumerFactory setConsumeEventListener(ConsumeEventListener consumeEventListener) {
-        this.consumeEventListener = consumeEventListener;
-        return this;
-    }
-
-    public ConsumerFactory setConsumerObserveOnScheduler(Scheduler consumerObserveOnScheduler) {
-        this.consumerObserveOnScheduler = consumerObserveOnScheduler;
-        return this;
-    }
-
-    public Observable<Message> createConsumer(String queue) {
-        log.infoWithParams("Creating publisher.",
-                "consumeChannels", settings.getNum_channels(),
-                "preFetch", settings.getPre_fetch_count(),
-                "consumeEventListener", consumeEventListener);
-
-        SingleChannelConsumer consumer = new SingleChannelConsumer(
-                channelFactory,
-                queue,
-                settings.getPre_fetch_count(),
-                settings.getConsumer_tag_prefix() + "{" + queue + "}-consumer",
-                settings.getRetry_count(),
-                settings.getClose_timeout_millis(),
-                consumerObserveOnScheduler,
-                consumeEventListener);
-        List<Observable<Message>> consumers = new ArrayList<>();
-        for(int i=0; i<settings.getNum_channels(); i++){
-            consumers.add(consumer.consume());
-        }
-        return Observable.merge(consumers);
-    }
-
-    private ConsumeEventListener getConsumeEventListener() {
-        return new NoopConsumeEventListener();
-    }
-
+    public Observable<Message> createConsumer(String queue);
 }

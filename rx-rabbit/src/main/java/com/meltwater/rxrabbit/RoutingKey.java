7f867512ffa2707169739ea1c37ec4a424b82e98
Karl Neyvaldt <karl.neyvaldt@meltwater.com>
2015-11-19 11:20:01 -0800

    toString, hashCode and equals methods on RoutingKey,Exchange and Payload

--- rx-rabbit/src/main/java/com/meltwater/rxrabbit/RoutingKey.java
+++ rx-rabbit/src/main/java/com/meltwater/rxrabbit/RoutingKey.java
@@ -10,4 +10,24 @@ public class RoutingKey {
     public RoutingKey(String value) {
         this.value = value;
     }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoutingKey that = (RoutingKey) o;
+        return !(value != null ? !value.equals(that.value) : that.value != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return value != null ? value.hashCode() : 0;
+    }
+
 }
